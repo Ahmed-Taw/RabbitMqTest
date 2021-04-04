3cadfa01799f221cfbe0fd5ca304d784732ed7de
tawfik <ahmed_a1442010@outlook.com>
2021-04-04 18:31:57 +0200

    first commit

---
+++ Receive/Program.cs
@@ -0,0 +1,63 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace Receive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+
+            Console.WriteLine(" Press [enter] to Listen to firstQueue.");
+            Console.ReadLine();
+
+            using (var connection = factory.CreateConnection())
+            using(var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "firstQueue", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+                var consumer = new EventingBasicConsumer(channel);
+
+                consumer.Received += Consumer_Received;
+
+                // after we created the consumer we should consume from the queue using it 
+
+                channel.BasicConsume("firstQueue", true, consumer);
+
+                
+            }
+            Console.WriteLine(" Press [enter] to Listen to SecondQueue.");
+            Console.ReadLine();
+
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "secondQueue", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+                var consumer = new EventingBasicConsumer(channel);
+
+                consumer.Received += Consumer_Received;
+
+                // after we created the consumer we should consume from the queue using it 
+
+                channel.BasicConsume("secondQueue", true, consumer);
+
+
+            }
+
+            Console.ReadLine();
+        }
+
+        private static void Consumer_Received(object sender, BasicDeliverEventArgs e)
+        {
+            var body = e.Body;
+            var messageString = Encoding.UTF8.GetString(body.ToArray());
+
+            Console.WriteLine($"[x] Receivied Message:  {messageString}");
+        }
+    }
+}
---
+++ Send/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace Send
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "firstQueue", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+                string messageString = "FirstMessageTest";
+
+                var messageBody = Encoding.UTF8.GetBytes(messageString);
+
+                channel.BasicPublish(exchange: "", routingKey: "firstQueue", mandatory: false, basicProperties: null,body:messageBody);
+                Console.WriteLine(" [x] Sent {0}", messageString);
+            }
+
+            Console.WriteLine(" Press [enter] to Send a Message in Second Queue.");
+            var stringMessageFromtheUser = Console.ReadLine();
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "secondQueue", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+                string messageString = stringMessageFromtheUser;
+
+                var messageBody = Encoding.UTF8.GetBytes(messageString);
+
+                channel.BasicPublish(exchange: "", routingKey: "secondQueue", mandatory: false, basicProperties: null, body: messageBody);
+                Console.WriteLine(" [x] Sent {0}", messageString);
+            }
+
+            Console.WriteLine(" Press [enter] Exit.");
+             Console.ReadLine();
+        }
+    }
+
+    //class Send
+    //{
+    //    public static void Main()
+    //    {
+    //        var factory = new ConnectionFactory() { HostName = "localhost" };
+
+    //        using (var connection = factory.CreateConnection())
+    //        using (var channel = connection.CreateModel())
+    //        {
+    //            channel.QueueDeclare(queue: "firstQueue", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+    //            string messageString = "FirstMessageTest";
+
+    //            var messageBody = Encoding.UTF8.GetBytes(messageString);
+
+    //            channel.BasicPublish(exchange: "", routingKey: "firstQueue", mandatory: false, basicProperties: null);
+    //            Console.WriteLine(" [x] Sent {0}", messageString);
+    //        }
+
+    //        Console.WriteLine(" Press [enter] to exit.");
+    //        Console.ReadLine();
+    //    }
+    //}
+
+}
